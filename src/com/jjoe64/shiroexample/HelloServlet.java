2b79f65f218595752b37aa1614a103cd3ca9a026
Jonas Gehring <g.jjoe64@googlemail.com>
2014-02-24 21:31:26 +0100

    initial project setiup

---
+++ src/com/jjoe64/shiroexample/HelloServlet.java
@@ -0,0 +1,42 @@
+package com.jjoe64.shiroexample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+
+		writer.println("<html>");
+		writer.println("<head><title>Hello World Servlet</title></head>");
+		writer.println("<body>");
+		writer.println("	<h1>Hello World from a Sevlet!XX</h1>");
+		writer.println("<body>");
+		writer.println("</html>");
+
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
